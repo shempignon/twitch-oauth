5988456f4034aecab1cbd22a67b16265fc21df19
Shempignon <nicolas@pointndots.com>
2020-01-11 15:55:04 +0100

    Fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,41 @@
+use reqwest::header::AUTHORIZATION;
+use reqwest::Client;
+use reqwest::Url;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use reqwest::Client;
-use reqwest::header::AUTHORIZATION;
-use reqwest::Url;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct ValidatedToken {
-  client_id: String,
-  login: Option<String>,
-  user_id: Option<String>,
-  scopes: Vec<String>,
+    client_id: String,
+    login: Option<String>,
+    user_id: Option<String>,
+    scopes: Vec<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct AppAccessToken {
-  access_token: String,
-  expires_in: usize,
-  scope: Option<Vec<String>>,
-  token_type: String,
+    access_token: String,
+    expires_in: usize,
+    scope: Option<Vec<String>>,
+    token_type: String,
 }
 
-pub async fn get_app_access_token(client_id: &str, client_secret: &str, scopes: Vec<String>) -> Result<AppAccessToken, Box<dyn std::error::Error>> {
-  let joinee_scopes = scopes.join(" ");
-  
-  let mut params = HashMap::new();
-  params.insert("grant_type", "client_credentials");
-  params.insert("client_id", client_id);
-  params.insert("client_secret", client_secret);
-  params.insert("scope", joinee_scopes.as_str());
+pub async fn get_app_access_token(
+    client_id: &str,
+    client_secret: &str,
+    scopes: Vec<String>,
+) -> Result<AppAccessToken, Box<dyn std::error::Error>> {
+    let joinee_scopes = scopes.join(" ");
 
-  let url = Url::parse_with_params("https://id.twitch.tv/oauth2/token", &params).unwrap();
+    let mut params = HashMap::new();
+    params.insert("grant_type", "client_credentials");
+    params.insert("client_id", client_id);
+    params.insert("client_secret", client_secret);
+    params.insert("scope", joinee_scopes.as_str());
 
-  let resp: AppAccessToken =  Client::new()
+    let url = Url::parse_with_params("https://id.twitch.tv/oauth2/token", &params).unwrap();
+
+    let resp: AppAccessToken = Client::new()
         .post(url)
         .send()
         .await?
@@ -42,10 +46,12 @@ pub async fn get_app_access_token(client_id: &str, client_secret: &str, scopes: 
     Ok(resp)
 }
 
-pub async fn validate_token(token: AppAccessToken) -> Result<ValidatedToken, Box<dyn std::error::Error>> {
-  let auth_header = format!("OAuth {}", token.access_token);
+pub async fn validate_token(
+    token: AppAccessToken,
+) -> Result<ValidatedToken, Box<dyn std::error::Error>> {
+    let auth_header = format!("OAuth {}", token.access_token);
 
-  let resp: ValidatedToken = Client::new()
+    let resp: ValidatedToken = Client::new()
         .get("https://id.twitch.tv/oauth2/validate")
         .header(AUTHORIZATION, auth_header)
         .send()
@@ -57,18 +63,17 @@ pub async fn validate_token(token: AppAccessToken) -> Result<ValidatedToken, Box
     Ok(resp)
 }
 
-pub async fn revoke_token(token: AppAccessToken, client_id: &str) -> Result<(), Box<dyn std::error::Error>> {
-  let mut params = HashMap::new();
-  params.insert("token", token.access_token.as_str());
-  params.insert("client_id", client_id);
+pub async fn revoke_token(
+    token: AppAccessToken,
+    client_id: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut params = HashMap::new();
+    params.insert("token", token.access_token.as_str());
+    params.insert("client_id", client_id);
 
-  let url = Url::parse_with_params("https://id.twitch.tv/oauth2/revoke", &params).unwrap();
+    let url = Url::parse_with_params("https://id.twitch.tv/oauth2/revoke", &params).unwrap();
 
-  Client::new()
-    .post(url)
-    .send()
-    .await?
-    .error_for_status()?;
+    Client::new().post(url).send().await?.error_for_status()?;
 
-  Ok(())
+    Ok(())
 }
